b07dac92f66daf81bd8d148fd1071986358b1bee
Bordonous <87366097+Bordonous@users.noreply.github.com>
2021-11-30 15:25:03 +0100

    Create bobrza.py

--- bobrza.py
+++ bobrza.py
@@ -10,7 +10,7 @@ from folium.plugins import Fullscreen, minimap
 
 st.set_page_config(layout="centered", initial_sidebar_state="expanded")
 #ładowanie pliku z punktami turystycznymi
-location=open("D:/streamlit/bobrza1.csv")
+location=f"D:/streamlit/bobrza1.csv"
 bobrza_locations = pd.read_csv(location)
 bobrza_locations = bobrza_locations[["nazwa_zasobu", "lat", "lon","icon","color","type"]]
 
