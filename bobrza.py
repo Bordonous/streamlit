375052b06fff665dd5dabec07a01b4148ef3be61
Bordonous <87366097+Bordonous@users.noreply.github.com>
2021-12-17 13:37:24 +0100

    Add files via upload

--- bobrza.py
+++ bobrza.py
@@ -246,7 +246,13 @@ p{
 st.markdown(footer,unsafe_allow_html=True)        
         
 
-
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
     
 
